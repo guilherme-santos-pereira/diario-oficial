ac9e5f3028d22896d32b37764e5f8da87c62cf25
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2023-07-28 17:09:03 -0300

    feat: Status Page

--- src/Services/services.tsx
+++ src/Services/services.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { PATH } from "../PATH";
 
 const username = sessionStorage.getItem("username");
@@ -6,46 +6,63 @@ const password = sessionStorage.getItem("password");
 const encodedCredentials = btoa(`${username}:${password}`);
 
 export const defaultHeaders = {
-  headers: {
-    Authorization: "Basic " + encodedCredentials,
-  },
+    headers: {
+        Authorization: "Basic " + encodedCredentials,
+    },
 };
 
 const services = {
-  getMe: async (body: { username: string; password: string }) => {
-    const headers =
-      body.username && body.password
-        ? {
-            headers: {
-              Authorization:
-                "Basic " + btoa(`${body.username}:${body.password}`),
-            },
-          }
-        : defaultHeaders;
-    console.log("headers:", headers);
-    return axios
-      .get(PATH.base + "/all-files/", headers)
-      .then((data: any) => {
-        if (body.username && body.password) {
-          sessionStorage.setItem("username", body.username);
-          sessionStorage.setItem("password", body.password);
-          sessionStorage.setItem(
-            "credentials",
-            btoa(`${body.username}:${body.password}`)
-          );
+    getMe: async (body: { username: string; password: string }) => {
+        const headers =
+            body.username && body.password
+                ? {
+                    headers: {
+                        Authorization:
+                            "Basic " + btoa(`${body.username}:${body.password}`),
+                    },
+                }
+                : defaultHeaders;
+        return axios
+            .get(PATH.base + "/all-files/", headers)
+            .then((data: any) => {
+                if (body.username && body.password) {
+                    sessionStorage.setItem("username", body.username);
+                    sessionStorage.setItem("password", body.password);
+                    sessionStorage.setItem(
+                        "credentials",
+                        btoa(`${body.username}:${body.password}`)
+                    );
+                }
+                return data;
+            })
+            .catch((err: any) => console.log(err));
+    },
+    getPublic: async () => {
+        return axios
+            .get(PATH.base + "/all-posts/", defaultHeaders)
+            .then((data: any) => {
+                return data;
+            })
+            .catch((err: any) => console.log(err));
+    },
+    doPost: async (body: FormData, username?: string, password?: string) => {
+        const encodedCredentials = btoa(`${username}:${password}`);
+        const headers: AxiosRequestConfig["headers"] = {};
+
+        if (username && password) {
+            headers.Authorization = "Basic " + encodedCredentials;
+        } else {
+            headers.Authorization = defaultHeaders.headers.Authorization;
         }
-        return data;
-      })
-      .catch((err: any) => console.log(err));
-  },
-  getPublic: async () => {
-    return axios
-      .get(PATH.base + "/all-posts/", defaultHeaders)
-      .then((data: any) => {
-        return data;
-      })
-      .catch((err: any) => console.log(err));
-  },
+
+        try {
+            const response = await axios.post(PATH.base + "/do-post/", body, { headers });
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    },
 };
 
 export default services;
